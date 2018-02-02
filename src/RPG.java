67cf4b07f14547797e4c6dca42e2c6d3e1de1938
Bryan Taylor <clearly.b.t@gmail.com>
2018-02-02 15:44:45 -0500

    adding base intellij layout with a main class

---
+++ src/RPG.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Bryan on 2/2/18.
+ */
+public class RPG extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
