bec3228d7581402614f6780e1a8d2a7672077a15
Bryan Taylor <clearly.b.t@gmail.com>
2018-02-04 20:07:58 -0500

    Adding validation code for global movement

--- src/controllers/GlobalMovementListener.java
+++ src/controllers/GlobalMovementListener.java
@@ -2,12 +2,74 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import models.Character;
+import models.GlobalLevel;
 
 /**
  * Created by Bryan on 2/4/18.
  */
 public class GlobalMovementListener implements EventHandler<ActionEvent> {
-    public boolean checkVaildMove() {
+
+    public boolean checkVaildMove(String move, Character character, GlobalLevel map) {
+        switch (move) {
+            case "UP": // 8
+                if(character.getGlobalPos().getY() - 1 >= 0) {
+                    return true;
+                }
+                break;
+
+            case "DOWN": // 2
+                if(character.getGlobalPos().getY() + 1 < map.getGlobalMap().length - 1) {
+                    return true;
+                }
+                break;
+
+            case "LEFT": // 4
+                if(character.getGlobalPos().getX() - 1 >= 0) {
+                    return true;
+                }
+                break;
+
+            case "RIGHT": //6
+                if(character.getGlobalPos().getX() + 1 < map.getGlobalMap()[0].length - 1) {
+                    return true;
+                }
+                break;
+
+            case "END": // 1 DOWN_LEFT
+                if(character.getGlobalPos().getY() + 1 < map.getGlobalMap().length - 1) {
+                    if(character.getGlobalPos().getX() - 1 >= 0) {
+                        return true;
+                    }
+                }
+                break;
+
+            case "PAGE_DOWN":  // DOWN_RIGHT
+                if(character.getGlobalPos().getY() + 1 < map.getGlobalMap().length - 1) {
+                    if(character.getGlobalPos().getX() + 1 < map.getGlobalMap()[0].length - 1) {
+                        return true;
+                    }
+                }
+                break;
+
+            case "HOME":  // UP_LEFT
+                if(character.getGlobalPos().getY() - 1 >= 0) {
+                    if(character.getGlobalPos().getX() - 1 >= 0) {
+                        return true;
+                    }
+                }
+                break;
+
+
+            case "PAGE_UP": // UP_RIGHT
+                if(character.getGlobalPos().getY() - 1 >= 0) {
+                    if(character.getGlobalPos().getX() + 1 < map.getGlobalMap()[0].length - 1) {
+                        return true;
+                    }
+                }
+                break;
+        }
+
         return false;
     }
 
