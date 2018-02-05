d46d368ed5369f70e5c787e6086149244a3aa392
Bryan Taylor <clearly.b.t@gmail.com>
2018-02-04 21:14:21 -0500

    Refactoring validate method and adding fucntionality to updating the character position

--- src/controllers/GlobalMovementListener.java
+++ src/controllers/GlobalMovementListener.java
@@ -5,75 +5,74 @@ import javafx.event.EventHandler;
 import models.Character;
 import models.GlobalLevel;
 
+import java.awt.*;
+
+
 /**
  * Created by Bryan on 2/4/18.
  */
 public class GlobalMovementListener implements EventHandler<ActionEvent> {
 
-    public boolean checkVaildMove(String move, Character character, GlobalLevel map) {
+    private boolean checkVaildMove(Point projectedMove, Character character, GlobalLevel map) {
+        Point characterPosition = character.getGlobalPos();
+        int xPositionChange = (int)(characterPosition.getX() + projectedMove.getX());
+        int yPositionChange = (int)(characterPosition.getY() + projectedMove.getY());
+
+        if(xPositionChange < 0 || xPositionChange > map.getGlobalMap()[0].length - 1)
+            return false;
+
+        else if(yPositionChange < 0 || yPositionChange > map.getGlobalMap().length - 1)
+            return false;
+
+        else
+            return true;
+    }
+
+    public void updateCharacterPosition(String move, Character character, GlobalLevel map) {
+        Point projectedMove;
+
         switch (move) {
             case "UP": // 8
-                if(character.getGlobalPos().getY() - 1 >= 0) {
-                    return true;
-                }
+                projectedMove = new Point(0, -1);
                 break;
 
             case "DOWN": // 2
-                if(character.getGlobalPos().getY() + 1 < map.getGlobalMap().length - 1) {
-                    return true;
-                }
+                projectedMove = new Point(0, 1);
                 break;
 
             case "LEFT": // 4
-                if(character.getGlobalPos().getX() - 1 >= 0) {
-                    return true;
-                }
+                projectedMove = new Point(-1, 0);
                 break;
 
             case "RIGHT": //6
-                if(character.getGlobalPos().getX() + 1 < map.getGlobalMap()[0].length - 1) {
-                    return true;
-                }
+                projectedMove = new Point(1, 0);
                 break;
 
             case "END": // 1 DOWN_LEFT
-                if(character.getGlobalPos().getY() + 1 < map.getGlobalMap().length - 1) {
-                    if(character.getGlobalPos().getX() - 1 >= 0) {
-                        return true;
-                    }
-                }
+                projectedMove = new Point(-1, 1);
                 break;
 
             case "PAGE_DOWN":  // DOWN_RIGHT
-                if(character.getGlobalPos().getY() + 1 < map.getGlobalMap().length - 1) {
-                    if(character.getGlobalPos().getX() + 1 < map.getGlobalMap()[0].length - 1) {
-                        return true;
-                    }
-                }
+                projectedMove = new Point(1, 1);
                 break;
 
             case "HOME":  // UP_LEFT
-                if(character.getGlobalPos().getY() - 1 >= 0) {
-                    if(character.getGlobalPos().getX() - 1 >= 0) {
-                        return true;
-                    }
-                }
+                projectedMove = new Point(-1, -1);
                 break;
 
 
             case "PAGE_UP": // UP_RIGHT
-                if(character.getGlobalPos().getY() - 1 >= 0) {
-                    if(character.getGlobalPos().getX() + 1 < map.getGlobalMap()[0].length - 1) {
-                        return true;
-                    }
-                }
+                projectedMove = new Point(1, -1);
                 break;
+
+            default:
+                projectedMove = new Point(0, 0);
         }
 
-        return false;
+        if(checkVaildMove(projectedMove, character, map)) {
+            character.updateGlobalPos(projectedMove);
+        }
     }
-
-    public void updateCharacterPosition() {}
 
     public boolean checkForLocalLevel() {
         return false;
