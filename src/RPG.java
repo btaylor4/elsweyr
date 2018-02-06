a0dd401f7c96d8556151179660dfb204d30b463a
jacobkissel <jacobkissel@ufl.edu>
2018-02-05 22:45:48 -0800

    Added buttons and keypress functionality to localgameplay view and controller as well as set the rpg main application to load straight into local gameplay view

--- src/RPG.java
+++ src/RPG.java
@@ -1,5 +1,10 @@
+import controllers.LocalGameplayController;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import models.GlobalLevel;
+import models.Zone;
+import views.LocalGameplayView;
+import models.Character;
 
 /**
  * Created by Bryan on 2/2/18.
@@ -8,7 +13,12 @@ public class RPG extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        //TODO: this is not the actual start logic, its just testing and showing the local view
+        LocalGameplayView localView = new LocalGameplayView();
+        Character playerCharacter = new Character();
+        Zone localLevel = new Zone();
+        LocalGameplayController localController = new LocalGameplayController(localView, playerCharacter, localLevel);
+        localView.show();
     }
 
     public static void main(String[] args) {
