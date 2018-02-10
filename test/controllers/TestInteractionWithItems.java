346627dc1bb2f8edd41942e49ebaea7b868a61ac
Thomas Headley <thomasheadleyjr@gmail.com>
2018-02-10 13:42:45 -0500

    Takeable and OneShot Items are now removed from map

--- test/controllers/TestInteractionWithItems.java
+++ test/controllers/TestInteractionWithItems.java
@@ -15,6 +15,7 @@ import java.awt.*;
 
 import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * Created by Bryan on 2/6/18.
@@ -101,6 +102,28 @@ public class TestInteractionWithItems extends ApplicationTest {
         movementHandler.handle(event);
         assertTrue(character.getInventory().hasItem(item));
         assertTrue(character.getInventory().getItems().contains(item));
+        assertNull(zones[0][0].getLocalMap()[0][1].getItem());
+    }
+
+    @Test
+    public void testOneShotItemIsActivatedAfterMove() {
+        item = new OneShotHealthPot(10);
+        item.setName("health pot");
+
+        character.setTotalHP(50);
+        character.setCurrentHP(40);
+
+        zones[0][0].getLocalMap()[0][1].setItem(item);
+
+        controller = new LocalGameplayController(localview, character, globalLevel);
+        movementHandler = controller.new MovementHandler();
+
+        KeyEvent event = new KeyEvent(KeyEvent.KEY_PRESSED, "", "", KeyCode.RIGHT, false,
+                false, false, false);
+
+        movementHandler.handle(event);
+        assertTrue(character.getCurrentHP() == 50);
+        assertNull(zones[0][0].getLocalMap()[0][1].getItem());
     }
 
     @Test
